dd21b46512c26b5e98332e1cc5e0b843b858449e
falak-n <nfalak524@gmail.com>
2025-02-11 23:12:47 +0530

    updating

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 const corsOptions={
-     origin:'http://localhost:5173',
+     origin:['http://localhost:5173','https://club-connect-smoky.vercel.app'],
     // origin:'https://clubconnect-4t81.onrender.com/',
     credentials:true
 }
