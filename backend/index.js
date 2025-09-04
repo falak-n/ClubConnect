e96c12c0b93c57eec774a3d94f7077f82ee936f8
falak naaz <134971594+falak-n@users.noreply.github.com>
2025-09-04 15:18:47 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -24,11 +24,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
-const corsOptions={
-     //origin:['http://localhost:5173','https://club-connect-smoky.vercel.app'],
-     origin:'https://clubconnect-1.onrender.com/',
-    credentials:true
-}
+const corsOptions = {
+    origin: [
+        'http://localhost:5173',
+        'https://club-connect-smoky.vercel.app'
+    ],
+    credentials: true
+};
 app.use(cors(corsOptions));
 
  
